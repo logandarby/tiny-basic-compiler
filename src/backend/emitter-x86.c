e7cf4be3de5f0e9f7adc2b40234fda903dbfabaa
logandarby <logandarby4@gmail.com>
2025-08-05 20:15:21 -0400

    Add control flow constructs (if and while) to emitter

--- src/backend/emitter-x86.c
+++ src/backend/emitter-x86.c
@@ -81,12 +81,37 @@ const char *INPUT_INTEGER = "input_integer";
 const char *LITERAL_DELIMITER = "_static_";
 const char *SYMBOL_DELIMITER = "_var_";
 const char *LABEL_DELIMITER = ".L";
+const char *INTERNAL_LABEL_DELIMITER = ".IL";
 
-void _emit_literals(FILE *file, const LiteralTable literals) {
+// ----------------------
+// Emitter Internals
+// ----------------------
+
+typedef struct {
+  FILE *output;
+  size_t control_flow_label; // Used to create unique labels for IF and WHILE
+                             // statement
+  VariableTable *table;      // Non-owning reference
+  AST *ast;                  // Non-owning reference
+} Emitter;
+
+Emitter emitter_init(FILE *file, AST *ast, VariableTable *table) {
+  return (Emitter){
+      .ast = ast,
+      .output = file,
+      .table = table,
+      .control_flow_label = 0,
+  };
+}
+
+size_t emitter_get_label(Emitter *emit) { return emit->control_flow_label++; }
+
+void _emit_literals(Emitter *emit) {
+  const LiteralTable literals = emit->table->literal_table;
   const size_t literal_len = shlenu(literals);
   for (size_t i = 0; i < literal_len; i++) {
     const LiteralHash lit = literals[i];
-    fprintf(file, "\t%s%ld: .string \"%s\"\n", LITERAL_DELIMITER,
+    fprintf(emit->output, "\t%s%ld: .string \"%s\"\n", LITERAL_DELIMITER,
             lit.value.label, lit.key);
   }
 }
@@ -95,28 +120,28 @@ void _emit_literals(FILE *file, const LiteralTable literals) {
 // var_name: .skip 4
 // This initialized 4 bytes of memory (DWORD) which can be referenced later
 // using mov DWORD PTR var_name[rip], 10
-void _emit_symbols(FILE *file, const SymbolTable symbol_table) {
+void _emit_symbols(Emitter *emit) {
+  const SymbolTable symbol_table = emit->table->symbol_table;
   const size_t symbol_len = (size_t)shlenu(symbol_table);
   for (size_t i = 0; i < symbol_len; i++) {
     const SymbolHash sym = symbol_table[i];
-    fprintf(file, "\t%s%s: .skip 8\n", SYMBOL_DELIMITER, sym.key);
+    fprintf(emit->output, "\t%s%s: .skip 8\n", SYMBOL_DELIMITER, sym.key);
   }
 }
 
 // Emits a primary identifier and places the result in rax
 // primary ::= number | indetifier
-void _emit_primary(FILE *file, AST *ast, NodeID primary_node,
-                   VariableTable *table) {
-  UNUSED(table);
+void _emit_primary(Emitter *emit, NodeID primary_node) {
+  AST *ast = emit->ast;
   const NodeID num_or_ident = ast_get_first_child(ast, primary_node);
   // can only be number or identifier
   if (num_or_ident == NO_NODE || ast_node_is_grammar(ast, num_or_ident))
     return;
   const Token *token = ast_node_get_token(ast, num_or_ident);
   if (token->type == TOKEN_NUMBER) {
-    fprintf(file, "\tmov rax, %s\n", token->text);
+    fprintf(emit->output, "\tmov rax, %s\n", token->text);
   } else if (token->type == TOKEN_IDENT) {
-    fprintf(file, "\tmov rax, QWORD PTR %s%s[rip]\n", SYMBOL_DELIMITER,
+    fprintf(emit->output, "\tmov rax, QWORD PTR %s%s[rip]\n", SYMBOL_DELIMITER,
             token->text);
   }
   // ERROR bad primary formed
@@ -124,8 +149,8 @@ void _emit_primary(FILE *file, AST *ast, NodeID primary_node,
 
 // Emits a unary identifier and places the result in rax
 // unary ::= ["+" | "-"] primary
-void _emit_unary(FILE *file, AST *ast, NodeID unary_node,
-                 VariableTable *table) {
+void _emit_unary(Emitter *emit, NodeID unary_node) {
+  AST *ast = emit->ast;
   // Should only be unary node, obv
   if (ast_node_is_token(ast, unary_node) ||
       ast_node_get_grammar(ast, unary_node) != GRAMMAR_TYPE_UNARY)
@@ -140,43 +165,44 @@ void _emit_unary(FILE *file, AST *ast, NodeID unary_node,
     if (primary_child == NO_NODE)
       return;
     if (token->type == TOKEN_MINUS) {
-      _emit_primary(file, ast, primary_child, table);
-      fprintf(file, "\tneg rax\n");
+      _emit_primary(emit, primary_child);
+      fprintf(emit->output, "\tneg rax\n");
       return;
     } else if (token->type == TOKEN_PLUS) {
       // Unary + is a noop
-      _emit_primary(file, ast, primary_child, table);
+      _emit_primary(emit, primary_child);
       return;
     }
   } else {
     // else, it must be a primary
-    _emit_primary(file, ast, first_child, table);
+    _emit_primary(emit, first_child);
   }
 }
 
 // Emits a term, and stores the result into rax
 // term ::= unary {( "/" | "*" ) unary}
-void _emit_term(FILE *file, AST *ast, NodeID term_node, VariableTable *table) {
+void _emit_term(Emitter *emit, NodeID term_node) {
+  AST *ast = emit->ast;
   NodeID child = ast_get_first_child(ast, term_node);
   if (child == NO_NODE)
     return;
-  _emit_unary(file, ast, child, table);
+  _emit_unary(emit, child);
   child = ast_get_next_sibling(ast, child);
   // Handle the {( "/" | "*" ) unary} case
   while (child != NO_NODE) {
     const NodeID unary_node = ast_get_next_sibling(ast, child);
     if (!ast_node_is_token(ast, child) || unary_node == NO_NODE)
       return;
-    fprintf(file, "\tpush rax\n");
-    _emit_unary(file, ast, unary_node, table);
-    fprintf(file, "\tmov rbx, rax\n"); // Move the unary node into rbx
-    fprintf(file, "\tpop rax\n");
+    fprintf(emit->output, "\tpush rax\n");
+    _emit_unary(emit, unary_node);
+    fprintf(emit->output, "\tmov rbx, rax\n"); // Move the unary node into rbx
+    fprintf(emit->output, "\tpop rax\n");
     const Token *child_token = ast_node_get_token(ast, child);
     if (child_token->type == TOKEN_MULT) {
-      fprintf(file, "\timul rax, rbx\n");
+      fprintf(emit->output, "\timul rax, rbx\n");
     } else if (child_token->type == TOKEN_DIV) {
-      fprintf(file, "\tcqo\n");
-      fprintf(file, "\tidiv rbx\n"); // stores result in rax
+      fprintf(emit->output, "\tcqo\n");
+      fprintf(emit->output, "\tidiv rbx\n"); // stores result in rax
     } else {
       return;
     }
@@ -186,27 +212,27 @@ void _emit_term(FILE *file, AST *ast, NodeID term_node, VariableTable *table) {
 
 // expression := term {( "-" | "+" ) term}
 // Emits an expression, and places the result in rax
-void _emit_expression(FILE *file, AST *ast, NodeID expr_node,
-                      VariableTable *table) {
+void _emit_expression(Emitter *emit, NodeID expr_node) {
+  AST *ast = emit->ast;
   NodeID child = ast_get_first_child(ast, expr_node);
   if (child == NO_NODE)
     return;
-  _emit_term(file, ast, child, table);
+  _emit_term(emit, child);
   child = ast_get_next_sibling(ast, child);
   // Handle the {( "/" | "*" ) unary} case
   while (child != NO_NODE) {
     const NodeID term_node = ast_get_next_sibling(ast, child);
     if (!ast_node_is_token(ast, child) || term_node == NO_NODE)
       return;
-    fprintf(file, "\tpush rax\n");
-    _emit_term(file, ast, term_node, table);
-    fprintf(file, "\tmov rbx, rax\n"); // Move the unary node into rbx
-    fprintf(file, "\tpop rax\n");
+    fprintf(emit->output, "\tpush rax\n");
+    _emit_term(emit, term_node);
+    fprintf(emit->output, "\tmov rbx, rax\n"); // Move the unary node into rbx
+    fprintf(emit->output, "\tpop rax\n");
     const Token *child_token = ast_node_get_token(ast, child);
     if (child_token->type == TOKEN_PLUS) {
-      fprintf(file, "\tadd rax, rbx\n");
+      fprintf(emit->output, "\tadd rax, rbx\n");
     } else if (child_token->type == TOKEN_MINUS) {
-      fprintf(file, "\tsub rax, rbx\n");
+      fprintf(emit->output, "\tsub rax, rbx\n");
     } else {
       return;
     }
@@ -214,8 +240,46 @@ void _emit_expression(FILE *file, AST *ast, NodeID expr_node,
   }
 }
 
-void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
-                     VariableTable *table) {
+// Stores the comparison result in the jump flag (after cmp). Returns the ID of
+// the operation node comparison ::= expression ("==" | "!=" | ">" | ">=" | "<"
+// | "<=") expression
+NodeID _emit_comparison(Emitter *emit, NodeID comparison_node) {
+  AST *ast = emit->ast;
+  NodeID left_expr = ast_get_first_child(ast, comparison_node);
+  NodeID op_node = ast_get_next_sibling(ast, left_expr);
+  NodeID right_expr = ast_get_next_sibling(ast, op_node);
+  if (left_expr == NO_NODE || op_node == NO_NODE || right_expr == NO_NODE)
+    return NO_NODE;
+  _emit_expression(emit, left_expr);
+  fprintf(emit->output, "\tpush rax\n");
+  _emit_expression(emit, right_expr);
+  fprintf(emit->output, "\tmov rbx, rax\n");
+  fprintf(emit->output, "\tpop rax\n");
+  fprintf(emit->output, "\tcmp rax, rbx\n");
+  return op_node;
+}
+
+const char *_get_jump_instruction_from_operation(const enum TOKEN token) {
+  // Switch statement isn't used bc gcc hates me
+  if (token == TOKEN_EQEQ)
+    return "jne";
+  if (token == TOKEN_NOTEQ)
+    return "je";
+  if (token == TOKEN_GT)
+    return "jle";
+  if (token == TOKEN_GTE)
+    return "jl";
+  if (token == TOKEN_LT)
+    return "jge";
+  if (token == TOKEN_LTE)
+    return "jg";
+  return NULL;
+}
+
+NodeID _emit_statement_block(Emitter *emit, NodeID possible_statement_node);
+
+void _emit_statement(Emitter *emit, NodeID statement_node) {
+  AST *ast = emit->ast;
   NodeID first_child = ast_get_first_child(ast, statement_node);
   if (first_child == NO_NODE || !ast_node_is_token(ast, first_child))
     return;
@@ -227,19 +291,19 @@ void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
     if (ast_node_is_token(ast, expr_or_str) &&
         ast_node_get_token(ast, expr_or_str)->type == TOKEN_STRING) {
       const Token *string = ast_node_get_token(ast, expr_or_str);
-      LiteralInfo literal = shget(table->literal_table, string->text);
-      fprintf(file, "\tlea rdi, %s%ld[rip]\n", LITERAL_DELIMITER,
+      LiteralInfo literal = shget(emit->table->literal_table, string->text);
+      fprintf(emit->output, "\tlea rdi, %s%ld[rip]\n", LITERAL_DELIMITER,
               literal.label);
-      fprintf(file, "\tcall %s\n", PRINT_STRING);
+      fprintf(emit->output, "\tcall %s\n", PRINT_STRING);
       return;
     }
     // Otherwise, check if it's an expression node
     if (ast_node_is_grammar(ast, expr_or_str) &&
         ast_node_get_grammar(ast, expr_or_str) == GRAMMAR_TYPE_EXPRESSION) {
       // Gets expr int in rax
-      _emit_expression(file, ast, expr_or_str, table);
-      fprintf(file, "\tmov rdi, rax\n");
-      fprintf(file, "\tcall %s\n", PRINT_INTEGER);
+      _emit_expression(emit, expr_or_str);
+      fprintf(emit->output, "\tmov rdi, rax\n");
+      fprintf(emit->output, "\tcall %s\n", PRINT_INTEGER);
       return;
     }
     // ERROR! Bad print statement
@@ -253,8 +317,8 @@ void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
       return;
     const Token *ident_token = ast_node_get_token(ast, ident_node);
     // Get identifier name and write value
-    _emit_expression(file, ast, expr_node, table);
-    fprintf(file, "\tmov QWORD PTR %s%s[rip], rax\n", SYMBOL_DELIMITER,
+    _emit_expression(emit, expr_node);
+    fprintf(emit->output, "\tmov QWORD PTR %s%s[rip], rax\n", SYMBOL_DELIMITER,
             ident_token->text);
     return;
   } else if (token->type == TOKEN_INPUT) {
@@ -264,8 +328,8 @@ void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
       return;
     const Token *ident_token = ast_node_get_token(ast, ident_node);
     DZ_ASSERT(ident_token->type == TOKEN_IDENT);
-    fprintf(file, "\tcall %s\n", INPUT_INTEGER);
-    fprintf(file, "\tmov QWORD PTR %s%s[rip], rax\n", SYMBOL_DELIMITER,
+    fprintf(emit->output, "\tcall %s\n", INPUT_INTEGER);
+    fprintf(emit->output, "\tmov QWORD PTR %s%s[rip], rax\n", SYMBOL_DELIMITER,
             ident_token->text);
     return;
   } else if (token->type == TOKEN_LABEL) {
@@ -275,7 +339,7 @@ void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
       return;
     const Token *ident_token = ast_node_get_token(ast, label_ident_node);
     DZ_ASSERT(ident_token->type == TOKEN_IDENT);
-    fprintf(file, "%s%s:\n", LABEL_DELIMITER, ident_token->text);
+    fprintf(emit->output, "%s%s:\n", LABEL_DELIMITER, ident_token->text);
     return;
   } else if (token->type == TOKEN_GOTO) {
     const NodeID label_ident_node = ast_get_next_sibling(ast, first_child);
@@ -283,37 +347,98 @@ void _emit_statement(FILE *file, AST *ast, NodeID statement_node,
       return;
     const Token *ident_token = ast_node_get_token(ast, label_ident_node);
     DZ_ASSERT(ident_token->type == TOKEN_IDENT);
-    fprintf(file, "\tjmp %s%s\n", LABEL_DELIMITER, ident_token->text);
+    fprintf(emit->output, "\tjmp %s%s\n", LABEL_DELIMITER, ident_token->text);
     return;
+  } else if (token->type == TOKEN_IF) {
+    // "IF" comparison "THEN" nl {statement}* "ENDIF" nl
+    const NodeID comp_node = ast_get_next_sibling(ast, first_child);
+    if (comp_node == NO_NODE)
+      return;
+    NodeID op_node = _emit_comparison(emit, comp_node);
+    const char *jmp_inst = _get_jump_instruction_from_operation(
+        ast_node_get_token(ast, op_node)->type);
+    const size_t label_number = emitter_get_label(emit);
+    fprintf(emit->output, "\t%s %s%ld\n", jmp_inst, INTERNAL_LABEL_DELIMITER,
+            label_number);
+    // Emit THEN body
+    const NodeID then_node = ast_get_next_sibling(ast, comp_node);
+    DZ_ASSERT(ast_node_get_token(ast, then_node)->type == TOKEN_THEN);
+    const NodeID possible_statement = ast_get_next_sibling(ast, then_node);
+    const NodeID end_if_node = _emit_statement_block(emit, possible_statement);
+    DZ_ASSERT(ast_node_get_token(ast, end_if_node)->type == TOKEN_ENDIF);
+    UNUSED(end_if_node);
+    fprintf(emit->output, "%s%ld:", INTERNAL_LABEL_DELIMITER, label_number);
+    return;
+  } else if (token->type == TOKEN_WHILE) {
+    // "WHILE" comparison "REPEAT" nl {statement}* "ENDWHILE" nl
+    const NodeID comp_node = ast_get_next_sibling(ast, first_child);
+    if (comp_node == NO_NODE)
+      return;
+    const size_t loop_start_label = emitter_get_label(emit);
+    const size_t loop_end_label = emitter_get_label(emit);
+    fprintf(emit->output, "%s%ld:\n", INTERNAL_LABEL_DELIMITER,
+            loop_start_label);
+    NodeID op_node = _emit_comparison(emit, comp_node);
+    const char *jmp_inst = _get_jump_instruction_from_operation(
+        ast_node_get_token(ast, op_node)->type);
+    fprintf(emit->output, "\t%s %s%ld\n", jmp_inst, INTERNAL_LABEL_DELIMITER,
+            loop_end_label);
+
+    const NodeID repeat_node = ast_get_next_sibling(ast, comp_node);
+    DZ_ASSERT(ast_node_get_token(ast, repeat_node)->type == TOKEN_REPEAT);
+    const NodeID endwhile_node =
+        _emit_statement_block(emit, ast_get_next_sibling(ast, repeat_node));
+    fprintf(emit->output, "\tjmp %s%ld\n", INTERNAL_LABEL_DELIMITER,
+            loop_start_label);
+    UNUSED(endwhile_node);
+    DZ_ASSERT(ast_node_get_token(ast, endwhile_node)->type == TOKEN_ENDWHILE);
+    fprintf(emit->output, "%s%ld:\n", INTERNAL_LABEL_DELIMITER, loop_end_label);
   }
 }
 
-void _emit_program(FILE *file, AST *ast, NodeID program_node,
-                   VariableTable *table) {
+// Evaluate the results from several statements. Starts with
+// possible_statement_node, and continues traversing its siblings until a
+// non-statement is found, or the end of the sibling list is reached.
+// Returns the next possible sibling
+NodeID _emit_statement_block(Emitter *emit, NodeID possible_statement_node) {
+  AST *ast = emit->ast;
+  while (possible_statement_node != NO_NODE &&
+         ast_node_is_grammar(ast, possible_statement_node) &&
+         ast_node_get_grammar(ast, possible_statement_node) ==
+             GRAMMAR_TYPE_STATEMENT) {
+    _emit_statement(emit, possible_statement_node);
+    possible_statement_node =
+        ast_get_next_sibling(ast, possible_statement_node);
+  }
+  return possible_statement_node;
+}
+
+void _emit_program(Emitter *emit, NodeID program_node) {
+  AST *ast = emit->ast;
   NodeID child = ast_get_first_child(ast, program_node);
   while (child != NO_NODE) {
-    _emit_statement(file, ast, child, table);
+    _emit_statement(emit, child);
     child = ast_get_next_sibling(ast, child);
   }
 }
 
 void emit_x86(FILE *file, AST *ast, VariableTable *table) {
-  UNUSED(ast);
-  fprintf(file, "%s", PREAMBLE);
+  Emitter emit = emitter_init(file, ast, table);
+  fprintf(emit.output, "%s", PREAMBLE);
   // Here's where the static vars should go
-  _emit_literals(file, table->literal_table);
-  _emit_symbols(file, table->symbol_table);
-  fprintf(file, "%s", MAIN_PREAMBLE);
-  fprintf(file, "%s", FUNC_PREAMBLE);
+  _emit_literals(&emit);
+  _emit_symbols(&emit);
+  fprintf(emit.output, "%s", MAIN_PREAMBLE);
+  fprintf(emit.output, "%s", FUNC_PREAMBLE);
   NodeID head = ast_head(ast);
   if (head != NO_NODE) {
-    _emit_program(file, ast, head, table);
+    _emit_program(&emit, head);
   }
   // Here's where the generated code should go
-  fprintf(file, "%s", FUNC_POSTAMBLE);
-  fprintf(file, "%s", PRINT_INT_HELPER);
-  fprintf(file, "%s", PRINT_STRING_HELPER);
-  fprintf(file, "%s", INPUT_INTEGER_HELPER);
-  fprintf(file, "%s", POSTAMBLE);
+  fprintf(emit.output, "%s", FUNC_POSTAMBLE);
+  fprintf(emit.output, "%s", PRINT_INT_HELPER);
+  fprintf(emit.output, "%s", PRINT_STRING_HELPER);
+  fprintf(emit.output, "%s", INPUT_INTEGER_HELPER);
+  fprintf(emit.output, "%s", POSTAMBLE);
   return;
 }
